72b2c31bb491bb16b7a0928880aca6a4198421cb
bPratik603 <bhosalepratik93@gmail.com>
2025-07-07 21:48:17 +0530

    import/export using array and object

--- 1_Basics/Math.js
+++ 1_Basics/Math.js
@@ -3,4 +3,27 @@ const add = (a,b)=>{
     return a+b;
 }
 
-module.exports = add;
+
+const sub = (a,b)=>{
+    return a-b;
+}
+
+
+const mul = (a,b)=>{
+    return a*b;
+}
+
+
+const div = (a,b)=>{
+    return a/b;
+}
+
+
+// module.exports = [add,sub,mul,div];
+
+module.exports = {
+    'add':add,
+    'sub':sub,
+    'mul':mul,
+    'div':div
+};
--- 1_Basics/user.js
+++ 1_Basics/user.js
@@ -1,4 +1,10 @@
 
-const add = require("./Math");
+const math = require('./Math');
 
-console.log(add(4,5));
+console.log(math.add(4,5));
+
+console.log(math.sub(4,5));
+
+console.log(math.mul(4,5));
+
+console.log(math.div(20,5));
