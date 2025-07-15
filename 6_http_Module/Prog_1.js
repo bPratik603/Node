6cae2e538abf4032a2650c00a59e3f2063c4e54e
bPratik603 <bhosalepratik93@gmail.com>
2025-07-15 12:02:12 +0530

    http module server create

---
+++ 6_http_Module/Prog_1.js
@@ -0,0 +1,26 @@
+// http module creating server
+
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+
+      if(req.url === "/"){
+         res.setHeader("Content-type","text/html");
+         res.write("<h1>This is home page</h1>");
+         res.end();
+      }else if(req.url === "/source-code"){
+         res.setHeader("Content-type","text/plain");
+         res.write("This is source code page");
+         res.end();
+      }else{
+         res.setHeader("Content-type","text/plain");
+         res.write("404 page not found");
+         res.end();
+      }
+})
+
+const PORT  = 3000;
+
+server.listen(PORT,()=>{
+   console.log("Listening on PORT "+PORT);
+})
